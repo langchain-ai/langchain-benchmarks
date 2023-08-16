d6fe6b4d425965f2d5e455a5bb95e94ec39a65bf
Lance Martin <lance@langchain.dev>
2023-08-15 17:21:46 -0700

    Update to use feedback

--- extraction/streamlit_app.py
+++ extraction/streamlit_app.py
@@ -1,48 +1,41 @@
-import json
-import pandas as pd
 import streamlit as st
+from langsmith import Client
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import create_extraction_chain
 
-# Text for extraction
-st.set_page_config(page_title='🦜🔗 OppenAI-mer: Text extraction on the Oppenheimer characters')
-
-from langsmith import Client
+st.set_page_config(page_title='🦜🔗 AutoGraph: Triple extraction')
 client = Client()
 def send_feedback(run_id, score):
     client.create_feedback(run_id, "user_score", score=score)
 
-st.title('🦜🔗 OppenAI-mer')
-st.info("LLMs are good at extracting structured output from natural lanaguge. Given a short bio of all the characters from the movie Oppenheimer, you can extract a structured summary of charecters and associated information about them using OpenAI functions and [LangChain](https://github.com/langchain-ai/langchain)")
+st.title('🦜🔗 AutoGraph: Triple extraction')
+st.info("LLMs are good at extracting structured output from natural lanaguge. This playground will extract knowledge graph triplets from the user input text using [OpenAI functions](https://openai.com/blog/function-calling-and-other-api-updates) and [LangChain](https://github.com/langchain-ai/langchain). Knowledge graph triplets help to represent relationships between entities in a structured manner.")
 
-# Show text
-with open("oppenheimer.txt", "r") as file:
+# Input text
+with open("oppenheimer_short.txt", "r") as file:
     oppenheimer_text = file.read()
-show_text_option = st.selectbox("Show Oppenheimer character bio?", ["No", "Yes"], index=0)
-if show_text_option == "Yes":
-    st.text_area("Oppenheimer Text:", value=oppenheimer_text, height=400, disabled=True)
 
 # LLM
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
 
-# Schema
+# Knowledge triplet schema
 default_schema = {
     "properties": {
-        "name": {"type": "string"},
-        "political_affiliation": {"type": "string"},
-        "scientific_contribution": {"type": "string"},
+        "subject": {"type": "string"},
+        "predicate": {"type": "string"},
+        "object": {"type": "string"},
     },
-    "required": ["name",],
+    "required": ["subject", "predicate", "object"],
 }
 
-# Convert the schema to a formatted string
-default_schema_str = json.dumps(default_schema, indent=4)
+# Create a text_area, set the default value to oppenheimer_text
+MAX_CHARS = 2000  # Maximum number of characters
+user_input_text = st.text_area("Enter your text (<2000 characters):", value=oppenheimer_text, height=200)
+if len(user_input_text) > MAX_CHARS:
+    st.warning(f"Text is too long. Only {MAX_CHARS} characters allowed!")
+    user_input_text = user_input_text[:MAX_CHARS]
 
-# Create a text_area in Streamlit and set the default value to the schema string
-# placeholder = 'Who was in cabin C128?'
-user_input_schema = st.text_area("Modify the JSON schema:", value=default_schema_str, height=200)
-
-# Output formatting
+# Output formatting of triples
 def json_to_markdown_table(json_list):
     if not json_list:
         return "No data available."
@@ -68,24 +61,11 @@ with st.form('myform', clear_on_submit=True):
 		
         with st.spinner('Calculating...'):
 
-            # Convert the user input string back to a JSON object
-            try:
-                modified_schema = json.loads(user_input_schema)
-                st.success("Successfully parsed the JSON schema!")
-            except json.JSONDecodeError:
-                st.error("Invalid JSON format. Please correct it and try again.")
-
-
             llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-            chain = create_extraction_chain(modified_schema, llm)
-            extraction_output = chain.run(oppenheimer_text)
-            # Convert the output to a string representation
-            # output_str = json.dumps(extraction_output, indent=4)
-            markdown_output = json_to_markdown_table(extraction_output)
-            run_id=1
-            # Display the output in a text box
-            # st.text_area("Extraction Output:", value=output_str, height=400, disabled=True)
-            # st.info(output_str)
+            chain = create_extraction_chain(default_schema, llm)
+            extraction_output = chain(user_input_text,include_run_info=True)
+            markdown_output = json_to_markdown_table(extraction_output['text'])
+            run_id = extraction_output["__run"].run_id
         
 # Feeback
 if markdown_output is not None:
