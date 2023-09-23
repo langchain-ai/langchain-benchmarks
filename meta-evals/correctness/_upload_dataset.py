3a4803fb5ad02f7248e89cb172b8df9f190d4b60
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-23 14:38:09 -0700

    Add other datasets

--- meta-evals/correctness/_upload_dataset.py
+++ meta-evals/correctness/_upload_dataset.py
@@ -1,13 +1,17 @@
-from pathlib import Path
-from langsmith import Client
+import concurrent.futures
 import json
 import logging
+from pathlib import Path
+
+from langsmith import Client
+
 logging.basicConfig(level=logging.INFO)
 
 # Synthetic dataset adapted from https://aclanthology.org/D13-1160/
 
 _DATA_REPO = Path(__file__).parent / "data"
 _CLIENT = Client()
+
 
 def _upload_dataset(path: str):
     with open(path, "r") as f:
@@ -16,15 +20,22 @@ def _upload_dataset(path: str):
         examples = data["examples"]
         try:
             dataset = _CLIENT.create_dataset(dataset_name)
-        except Exception as e:
-            logging.warning(f"Skipping {dataset_name}", e)
+        except Exception:
+            logging.warning(f"Skipping {dataset_name}")
             return
         logging.info(f"Uploading dataset: {dataset_name}")
-        for i, example in enumerate(examples):
-            _CLIENT.create_example(example["inputs"], dataset_id=dataset.id, outputs=example["outputs"])
-            print(f"Uploaded {i+1}/{len(examples)}", end="\r")
-    
-if __name__ == '__main__':
-    for dataset in _DATA_REPO.glob("*.json"):
-        _upload_dataset(dataset)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            executor.map(
+                lambda x: _CLIENT.create_example(x[0], dataset_id=x[1], outputs=x[2]),
+                zip(
+                    [example["inputs"] for example in examples],
+                    [dataset.id] * len(examples),
+                    [example["outputs"] for example in examples],
+                ),
+            )
 
+
+if __name__ == "__main__":
+    for dataset in _DATA_REPO.glob("*.json"):
+        print("Uploading dataset:", dataset)
+        _upload_dataset(dataset)
--- meta-evals/correctness/test_correctness_evaluator.py
+++ meta-evals/correctness/test_correctness_evaluator.py
@@ -1,15 +1,14 @@
 # Tests for the criteria evaluator
-from typing import Optional, Tuple
+from typing import Tuple
 from uuid import uuid4
 
 import pytest
-from langchain.callbacks.manager import CallbackManager
+from langchain import hub
+from langchain import chat_models, llms
 from langchain.evaluation import load_evaluator
-from langchain.load import dump as langchain_dump
 from langchain.schema import runnable
-from langchain.smith import RunEvalConfig
+from langchain.smith import RunEvalConfig, run_on_dataset
 from langsmith import Client, EvaluationResult
-from langchain.prompts import PromptTemplate
 from langsmith.evaluation import RunEvaluator
 from langsmith.schemas import Example, Run
 
@@ -36,6 +35,7 @@ class AbsDistanceEvaluator(RunEvaluator):
             score=abs(predicted_score - example.outputs["output_correctness_score"]),
         )
 
+
 class NullScore(RunEvaluator):
     def __init__(self) -> None:
         super().__init__()
@@ -45,217 +45,163 @@ class NullScore(RunEvaluator):
         null_score = 1 if predicted_score is None else 0
         return EvaluationResult(key="null_score", score=null_score)
 
+
 @pytest.fixture(scope="session")
 def uid() -> str:
     return uuid4().hex[:8]
 
 
-class EvaluatorRunnable(runnable.Runnable):
-    # We're going through the non-invoke API of the evaluator
-    # so to maintain nesting, we are copying some of the invoke code.
-    # This is so that the full trace isn't separated from the runnable.
-    def __init__(self, eval_chain) -> None:
-        super().__init__()
-        self._eval_chain = eval_chain
-
-    def invoke(
-        self, input: dict, config: Optional[runnable.RunnableConfig] = None
-    ) -> dict:
-        config = config or {}
-        callback_manager = CallbackManager.configure(
-            inheritable_callbacks=config.get("callbacks"),
-            inheritable_tags=config.get("tags"),
-            inheritable_metadata=config.get("metadata"),
-        )
-        run_manager = callback_manager.on_chain_start(
-            langchain_dump.dumpd(self),
-            input if isinstance(input, dict) else {"input": input},
-            run_type="chain",
-        )
-        try:
-            output = self._eval_chain.evaluate_strings(
-                input=input["input"],
-                prediction=input["input_prediction"],
-                reference=input["input_answer"],
-                callbacks=run_manager.get_child(),
-            )
-        except Exception as e:
-            run_manager.on_chain_error(e)
-            raise
-        else:
-            output_for_tracer = langchain_dump.dumpd(output)
-            run_manager.on_chain_end(
-                output_for_tracer
-                if isinstance(output_for_tracer, dict)
-                else {"output": output_for_tracer}
-            )
-            return output
-
-
-async def _check_dataset(
-    loader_kwargs: dict, dataset_name: str, project_name: str, tags: list
+def _check_dataset(
+    loader_kwargs: dict,
+    dataset_name: str,
+    project_name: str,
+    model_provider: str,
+    model_name: str,
+    tags: list,
+    metadata: dict,
 ) -> Tuple[float, float]:
     client = Client()
-    eval_chain = load_evaluator(**loader_kwargs)
-    to_evaluate = EvaluatorRunnable(eval_chain=eval_chain)
+    match model_provider:
+        case "openai":
+            llm = chat_models.ChatOpenAI(model_name=model_name, temperature=0)
+        case "openai-completion":
+            llm = llms.OpenAI(model_name=model_name, temperature=0)
+        case "anthropic":
+            llm = chat_models.ChatAnthropic(
+                model_name=model_name, max_tokens=1000, temperature=0
+            )
+    eval_chain = load_evaluator(**loader_kwargs, llm=llm)
 
-    res = await client.arun_on_dataset(
+    def to_evaluate(input: dict, config: runnable.RunnableConfig) -> dict:
+        return eval_chain.evaluate_strings(
+            input=input["input"],
+            prediction=input["input_prediction"],
+            reference=input["input_answer"],
+            **config,
+        )
+
+    res = run_on_dataset(
         dataset_name=dataset_name,
-        llm_or_chain_factory=to_evaluate,
-        evaluation=RunEvalConfig(
-            custom_evaluators=[ExactScoreMatch(), AbsDistanceEvaluator(), NullScore()],
+        llm_or_chain_factory=runnable.RunnableLambda(to_evaluate).with_config(
+            {"metadata": metadata}
         ),
+        evaluation=RunEvalConfig(
+            custom_evaluators=[
+                ExactScoreMatch(),
+                #   AbsDistanceEvaluator(),
+                NullScore(),
+            ],
+        ),
+        concurrency_level=8,
+        client=client,
         verbose=True,
         project_name=project_name,
         tags=["int-test"] + tags,
     )
-    feedback = list(client.list_feedback(
-        run_ids=[r.id for r in client.list_runs(project_name=res["project_name"])]
-    ))
-    scores = [
-        f.score
-        for f in feedback
-        if f.key == "exact_score_match" and f.score is not None
+    df = res.to_dataframe()
+    feedback_cols = [
+        col for col in df.columns if col not in ["input", "output", "reference"]
     ]
-    # assert len(scores) == 100
-    distance_scores = [
-        f.score
-        for f in feedback
-        if f.key == "absolute_distance" and f.score is not None
-    ]
-    # assert len(distance_scores) == 100
-    null_pred_scores = [
-        f.score
-        for f in feedback
-        if f.key == "null_score" and f.score is not None
-    ]
-    avg_score = sum(scores) / len(scores)
-    avg_distance_score = sum(distance_scores) / len(distance_scores)
-    avg_null_pred_score = sum(null_pred_scores) / len(null_pred_scores)
-    return {"exact_score_match": avg_score, "absolute_distance": avg_distance_score, "null_predictions": avg_null_pred_score, "lengths": [len(scores), len(distance_scores), len(null_pred_scores)]}
+    # Return averages
+    return df[feedback_cols].mean()
 
 
-def _get_project_name(loader_kwargs: dict, uid: str) -> str:
+def _get_project_name(
+    loader_kwargs: dict, uid: str, dataset_name: str, model: Tuple[str]
+) -> str:
     other_args = "-".join(f"[{k}={v}]" for k, v in loader_kwargs.items())
-    return f"{loader_kwargs['evaluator']}{other_args} - {uid}"
+    return f"{model[0]}.{model[1]}-{loader_kwargs['evaluator']}{other_args} - {dataset_name} - {uid}"
 
-template_variants = [
-"""Evaluate the submission for the following task:
-<input>
-{input}
-</input>
-<submission>
-{output}
-</submission>
-<reference>
-{reference}
-</reference>
 
-Criterion to evaluate: {criteria}
+# prompt = "wfh/criteria_candidates"
+# commits = [
+#     "f470538b",
+#     "c92fcf90",
+# ]
 
-1. Compare the submission with the reference.
-2. Determine if the submission meets the criterion.
-3. Explain your reasoning in a step-by-step manner.
-4. Print the single character "Y" or "N" to represent whether the submission meets the criterion.
-5. Repeat the character on a new line.
+# anthropic_prompt = "wfh/criteria_candidates_anthropic"
 
-Y/N
-""",
-"""Compare the following submission with the reference for the given task:
-<input>
-{input}
-</input>
-<submission>
-{output}
-</submission>
-<reference>
-{reference}
-</reference>
-
-Criteria: {criteria}
-
-1. Analyze the submission against the reference considering the above criteria.
-2. Detail your reasoning in a clear and logical sequence.
-3. Print "Y" if the submission meets the criteria, or "N" if it does not.
-4. Repeat the character on a new line.
-
-Y/N
-""",
-"""You are to assess whether a submission satisfies a specified criterion:
-<input>
-{input}
-</input>
-<submission>
-{output}
-</submission>
-<criteria>
-{criteria}
-</criteria>
-<reference>
-{reference}
-</reference>
-
-Does the submission meet the criterion? Follow these steps:
-
-1. Analyze the submission in comparison with the reference.
-2. Determine if it satisfies the given criterion.
-3. Detail your reasoning in a step-by-step manner.
-4. Print "Y" if the submission meets the criterion, or "N" if it does not, and repeat the character on a new line.
-
-Y/N
-"""
+prompt_list = [
+    {
+        "openai": "wfh/criteria_candidates:f470538b",
+        "anthropic": "wfh/criteria_candidates_anthropic:fb037730",
+    },  # Here we'll try the anthropic one that inserts the "reasoning" step in the AI's mouth
+    {
+        "openai": "wfh/criteria_candidates:c92fcf90",
+    },  # It's the same as the openai one
 ]
 
-@pytest.mark.parametrize(
-    "loader_kwargs",
-    [
-        # {"evaluator": "cot_qa"},
-        # {"evaluator": "qa"},
-        # {"evaluator": "labeled_criteria", "criteria": "correctness"},
-        # {"evaluator": "labeled_criteria", "criteria": "correctness", "strategy": "confidence"},
-        # {"evaluator": "labeled_criteria", "criteria": "correctness", "strategy": "score"},
 
-    ] + [
-        {"evaluator": "labeled_criteria", "criteria": "correctness", "template": i}
-        for i in range(len(template_variants))
+@pytest.mark.parametrize(
+    "dataset_name",
+    [
+        "Web Q&A Dataset - Incorrect",
+        "Carb-IE-Test INCORRECT",
+        "Opus100 - Incorrect",
+    ]
+    + [
+        "Web Q&A Dataset - Correct",
+        "Opus100 - Correct",
+        "Carb-IE-Test CORRECT",
     ],
 )
-@pytest.mark.asyncio
-async def test_metaeval_correctness(loader_kwargs: dict, uid: str):
-    # Should have >= 0.99 correctness
-    dataset_name = "Web Q&A Dataset - Correct"
-    project_name = _get_project_name(loader_kwargs, uid)
-    prompt = PromptTemplate.from_template(template=template_variants[loader_kwargs.pop("template")])
-    loader_kwargs["prompt"] = prompt
-    scores = await _check_dataset(
-        loader_kwargs, dataset_name, project_name, tags=["test_metaeval_correctness"]
-    )
-    score, distance_score, null_pred_score = scores["exact_score_match"], scores["absolute_distance"], scores["null_predictions"]
-    assert score >= 0.99
-    assert distance_score <= 0.1
-    assert null_pred_score == 0
-    assert all(scores["lengths"] == 100 for scores in scores.values())
-
-
+@pytest.mark.parametrize(
+    "model",
+    [
+        ("openai", "gpt-4"),
+        ("openai", "gpt-3.5-turbo"),
+        ("openai-completion", "gpt-3.5-turbo-instruct"),
+        ("anthropic", "claude-2"),
+    ],
+)
 @pytest.mark.parametrize(
     "loader_kwargs",
     [
         {"evaluator": "cot_qa"},
         {"evaluator": "qa"},
-        {"evaluator": "labeled_criteria", "criteria": "correctness"},
+    ]
+    + [
+        {
+            "evaluator": "labeled_criteria",
+            "criteria": "correctness",
+            "prompt_lookup": pl,
+        }
+        for pl in prompt_list
     ],
 )
 @pytest.mark.asyncio
-@pytest.mark.skip(reason="Already passes 100% so don't need to test as frequently.")
-async def test_metaeval_incorrectness(loader_kwargs: dict, uid: str):
-    # Expect  100% to be labeled as incorrect
-    dataset_name = "Web Q&A Dataset - Incorrect"
-    project_name= _get_project_name(loader_kwargs, uid)
-    scores = await _check_dataset(
-        loader_kwargs, dataset_name, project_name, tags=["test_metaeval_incorrectness"]
+async def test_metaeval_correctness(
+    loader_kwargs: dict, uid: str, dataset_name: str, model: Tuple[str, str]
+):
+    project_name = _get_project_name(loader_kwargs, uid, dataset_name, model)
+    tags = ["test_metaeval_correctness", loader_kwargs["evaluator"]]
+    metadata = {
+        "model_provider": model[0],
+        "model_name": model[1],
+        "dataset_name": dataset_name,
+        "evaluator": loader_kwargs["evaluator"],
+        "uid": str(uid),
+    }
+    if "prompt_lookup" in loader_kwargs:
+        prompt_lookup = loader_kwargs.get("prompt_lookup")
+        prompt_repo = prompt_lookup.get(
+            model[0], prompt_lookup.get("openai")
+        )  # Fall back on openai prompt
+        commit = prompt_repo.split(":")[-1]
+        prompt = hub.pull(prompt_repo)
+        loader_kwargs["prompt"] = prompt
+        tags += [commit]
+        print("Using prompt:", prompt_repo, commit)
+        metadata["prompt"] = prompt_repo
+        metadata["commit"] = commit
+    scores = _check_dataset(
+        loader_kwargs,
+        dataset_name,
+        project_name,
+        model_provider=model[0],
+        model_name=model[1],
+        tags=tags,
+        metadata=metadata,
     )
-    score, distance_score, null_pred_score = scores["exact_score_match"], scores["absolute_distance"], scores["null_predictions"]
-    assert score >= 1
-    assert distance_score <= 0.1
-    assert null_pred_score == 0
-    assert all(scores["lengths"] == 100 for scores in scores.values())
+    score = scores["exact_score_match"]
+    assert score >= 0.95
