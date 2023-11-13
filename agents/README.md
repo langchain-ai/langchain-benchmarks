15f81cf7fa6311f0bdec75e8bb05f60dd002a24c
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-13 16:43:39 -0500

    x

---
+++ agents/README.md
@@ -0,0 +1,7 @@
+# Cognitive Architecture
+
+Agent
+
+# Datasets
+
+* The following contains definitions of environments with which agents can be evaluated.
---
+++ agents/environments/giggling_cat.py
@@ -0,0 +1,7 @@
+"""A simple environment for evaluating an agent.
+
+This is an environment that contains a bunch of fake tools.
+
+The purpose of this environment is to evaluate an agent's ability to use
+these tools to solve problems.
+"""
