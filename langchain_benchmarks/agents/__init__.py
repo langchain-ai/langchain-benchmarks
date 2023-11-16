7b14c4c015d28acd3f84eae6d5e4cecf35cfcaae
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-16 09:21:34 -0500

    x

--- langchain_benchmarks/agents/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Package for helping to evaluate agent runs."""
--- tests/agents/test_agents.py
+++
@@ -1,3 +0,0 @@
-def test_import_agents() -> None:
-    """Test that all agents can be imported"""
-    from langchain_benchmarks.agents import environments, evaluators  # noqa: F401
