191bcf4f732f3050f198b48bc739c3a63aedeec3
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-16 15:57:39 -0500

    x

--- tests/unit_tests/test_public_api.py
+++ tests/unit_tests/test_public_api.py
@@ -5,8 +5,4 @@ def test_public_api() -> None:
     """Test that the public API is correct."""
     # This test will also fail if __all__ is not sorted.
     # Please keep it sorted!
-    assert __all__ == sorted(
-        [
-            "clone_dataset",
-        ]
-    )
+    assert __all__ == sorted(["clone_public_dataset", "download_public_dataset"])
