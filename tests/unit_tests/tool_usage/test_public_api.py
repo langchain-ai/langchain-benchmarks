8a72ff762958ab9ee818f44a3bb6cb77316f9a48
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-17 17:25:44 -0500

    x

--- tests/unit_tests/tool_usage/test_public_api.py
+++ tests/unit_tests/tool_usage/test_public_api.py
@@ -5,4 +5,4 @@ def test_public_api() -> None:
     """Test that the public API is correct."""
     # This test will also fail if __all__ is not sorted.
     # Please keep it sorted!
-    assert __all__ == sorted(["registry", "STANDARD_AGENT_EVALUATOR"])
+    assert __all__ == sorted(["registry", "STANDARD_AGENT_EVALUATOR"], key=str.lower)
