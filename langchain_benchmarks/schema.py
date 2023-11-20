f79d7972bfa4206aa0c6341e11cb0e15bd32e36b
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-20 14:03:49 -0500

    Add extraction task (#29)
    Add extraction task

--- langchain_benchmarks/schema.py
+++ langchain_benchmarks/schema.py
@@ -1,13 +1,14 @@
 """Schema for the Langchain Benchmarks."""
 import dataclasses
-from typing import List, Callable, Any, Optional
+from typing import List, Callable, Any, Optional, Type
 
 from langchain.tools import BaseTool
+from pydantic import BaseModel
 from tabulate import tabulate
 
 
 @dataclasses.dataclass(frozen=True)
-class Environment:
+class ToolUsageEnvironment:
     """An instance of an environment for tool usage."""
 
     tools: List[BaseTool]
@@ -18,8 +19,8 @@ class Environment:
 
 
 @dataclasses.dataclass(frozen=True)
-class Task:
-    """A definition for a task."""
+class BaseTask:
+    """A definition of a task."""
 
     id: int
     """The ID of the environment."""
@@ -28,31 +29,24 @@ class Task:
 
     dataset_id: str
     """The ID of the langsmith public dataset.
-    
+
     This dataset contains expected inputs/outputs for the environment, and
     can be used to evaluate the performance of a model/agent etc.
     """
 
-    create_environment: Callable[
-        [], Environment
-    ]  # Specialized for tool usage; refactor potentially
-    """Factory that returns an environment."""
-
     description: str
     """Description of the task for a data science practitioner.
-    
+
     This can contain information about the task, the dataset, the tools available
     etc.
     """
-
-    instructions: str
-    """Instructions for the agent/chain/llm."""
 
     def _repr_html_(self) -> str:
         """Return an HTML representation of the environment."""
         table = [
             ["ID", self.id],
             ["Name", self.name],
+            ["Type", self.__class__.__name__],
             ["Dataset ID", self.dataset_id],
             ["Description", self.description[:100] + "..."],
         ]
@@ -60,3 +54,22 @@ class Task:
             table,
             tablefmt="html",
         )
+
+
+@dataclasses.dataclass(frozen=True)
+class ToolUsageTask(BaseTask):
+    """A definition for a task."""
+
+    create_environment: Callable[[], ToolUsageEnvironment]
+    """Factory that returns an environment."""
+
+    instructions: str
+    """Instructions for the agent/chain/llm."""
+
+
+@dataclasses.dataclass(frozen=True)
+class ExtractionTask(BaseTask):
+    """A definition for an extraction task."""
+
+    model: Type[BaseModel]
+    """Get the model for the task."""
