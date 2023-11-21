814713a9e103ad572425e4d64ee7cc581c3dc11e
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-21 14:57:07 -0500

    x

--- langchain_benchmarks/tool_usage/tasks/type_writer.py
+++ langchain_benchmarks/tool_usage/tasks/type_writer.py
@@ -151,7 +151,7 @@ def _create_dataset() -> None:
                 "question": example["question"],
             },
             outputs={
-                "output": example["state"],
+                "reference": example["state"],
                 "expected_steps": example["expected_steps"],
                 "state": example["state"],
             },
