c1b0cf9851025d796182403a9d0a27b40044b257
William FH <13333726+hinthornw@users.noreply.github.com>
2023-11-21 15:25:09 -0800

    Add retrieval intro; Add links (#59)
    - Adds links so you can more easily click and navigate
    - Adds intro describing rag task and schema

--- langchain_benchmarks/rag/tasks/semi_structured_earnings/task.py
+++ langchain_benchmarks/rag/tasks/semi_structured_earnings/task.py
@@ -6,7 +6,8 @@ from langchain_benchmarks.rag.tasks.semi_structured_earnings.indexing.retriever_
 )
 from langchain_benchmarks.schema import RetrievalTask
 
-DATASET_ID = "c47d9617-ab99-4d6e-a6e6-92b8daf85a7d"  # ID of public Semi-structured Earnings dataset
+# ID of public Semi-structured Earnings dataset
+DATASET_ID = "https://smith.langchain.com/public/c47d9617-ab99-4d6e-a6e6-92b8daf85a7d/d"
 
 SEMI_STRUCTURED_EARNINGS_TASK = RetrievalTask(
     name="Semi-structured Earnings",
