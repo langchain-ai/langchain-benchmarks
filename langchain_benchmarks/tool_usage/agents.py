e469abbf3a3ecfdd52a4ca1403cf8a60149d883a
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-27 15:34:45 -0500

    x

--- langchain_benchmarks/tool_usage/agents.py
+++ langchain_benchmarks/tool_usage/agents.py
@@ -1,12 +1,14 @@
 """Code for creating an agent factory for evaluating tool usage tasks."""
-from typing import Any
+from typing import Any, Callable, Optional, List
 
 from langchain.agents import AgentExecutor
 from langchain.agents.format_scratchpad import format_to_openai_functions
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.language_model import BaseLanguageModel
 from langchain.schema.runnable import Runnable, RunnablePassthrough
+from langchain.tools.base import BaseTool
 from langchain.tools.render import format_tool_to_openai_function
 
 from langchain_benchmarks.schema import ToolUsageTask
@@ -17,6 +19,24 @@ def _ensure_output_exists(inputs: dict) -> dict:
     if "output" not in inputs:
         return {"output": "", **inputs}
     return inputs
+
+
+def _standardize_output(
+    runnable: Runnable, state_reader: Optional[Callable]
+) -> Runnable:
+    """Update the output of the runnable."""
+
+    def _read_state(*args: Any, **kwargs: Any) -> Any:
+        """Read the state of the environment."""
+        if state_reader is not None:
+            return state_reader()
+        else:
+            return None
+
+    runnable = runnable | _ensure_output_exists
+    if state_reader is not None:
+        runnable = runnable | RunnablePassthrough.assign(state=_read_state)
+    return runnable
 
 
 class OpenAIAgentFactory:
@@ -67,28 +87,35 @@ class OpenAIAgentFactory:
             | OpenAIFunctionsAgentOutputParser()
         )
 
-        def _read_state(*args: Any, **kwargs: Any) -> Any:
-            """Read the state of the environment."""
-            if env.read_state is not None:
-                return env.read_state()
-            else:
-                return None
-
-        runnable = (
-            AgentExecutor(
-                agent=runnable_agent,
-                tools=env.tools,
-                handle_parsing_errors=True,
-                return_intermediate_steps=True,
-            )
-            | _ensure_output_exists
+        runnable = AgentExecutor(
+            agent=runnable_agent,
+            tools=env.tools,
+            handle_parsing_errors=True,
+            return_intermediate_steps=True,
         )
 
-        if env.read_state is not None:
-            # If the environment has a state reader, add it to the runnable
-            runnable = runnable | RunnablePassthrough.assign(state=_read_state)
-
-        return runnable
+        # Returns `state` in the output if the environment has a state reader
+        # makes sure that `output` is always in the output
+        return _standardize_output(runnable, env.read_state)
 
     def __call__(self) -> Runnable:
         return self.create()
+
+
+def get_xml_agent(
+    model: BaseLanguageModel, tools: List[BaseTool], system_message: str
+) -> Runnable:
+    """Create an agent that uses the XML format for chat history."""
+    from langchain.agents import XMLAgent
+    from langchain.chains import LLMChain
+
+    chain = LLMChain(
+        llm=model,
+        prompt=XMLAgent.get_default_prompt(),
+        output_parser=XMLAgent.get_default_output_parser(),
+    )
+    agent = XMLAgent(tools=tools, llm_chain=chain)
+
+    return AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+
