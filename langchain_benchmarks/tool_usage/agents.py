140085b507e07c80786f864d56795e11a885cc77
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-27 23:06:39 -0500

    x

--- langchain_benchmarks/tool_usage/agents.py
+++ langchain_benchmarks/tool_usage/agents.py
@@ -1,14 +1,12 @@
 """Code for creating an agent factory for evaluating tool usage tasks."""
-from typing import Any, Callable, Optional, List
+from typing import Any, Callable, Optional
 
 from langchain.agents import AgentExecutor
 from langchain.agents.format_scratchpad import format_to_openai_functions
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema.language_model import BaseLanguageModel
-from langchain.schema.runnable import Runnable, RunnablePassthrough
-from langchain.tools.base import BaseTool
+from langchain.schema.runnable import Runnable, RunnableLambda, RunnablePassthrough
 from langchain.tools.render import format_tool_to_openai_function
 
 from langchain_benchmarks.schema import ToolUsageTask
@@ -24,7 +22,9 @@ def _ensure_output_exists(inputs: dict) -> dict:
 # PUBLIC API
 
 
-class OpenAIAgentFactory:
+# Retained for backwards compatibility for now
+# Consider deprecated
+class OpenAIAgentFactory:  # Deprecated
     def __init__(
         self, task: ToolUsageTask, *, model: str = "gpt-3.5-turbo-16k"
     ) -> None:
@@ -81,39 +81,28 @@ class OpenAIAgentFactory:
 
         # Returns `state` in the output if the environment has a state reader
         # makes sure that `output` is always in the output
-        return standardize_executor(runnable, state_reader=env.read_state)
+        return apply_agent_executor_adapter(runnable, state_reader=env.read_state)
 
     def __call__(self) -> Runnable:
         return self.create()
 
 
-def get_xml_agent(
-    model: BaseLanguageModel, tools: List[BaseTool], system_message: str
-) -> Runnable:
-    """Create an agent that uses the XML format for chat history."""
-    from langchain.agents import XMLAgent
-    from langchain.chains import LLMChain
-
-    chain = LLMChain(
-        llm=model,
-        prompt=XMLAgent.get_default_prompt(),
-        output_parser=XMLAgent.get_default_output_parser(),
-    )
-    agent = XMLAgent(tools=tools, llm_chain=chain)
-
-    return AgentExecutor(agent=agent, tools=tools, verbose=True)
+# PUBLIC API
 
 
-def standardize_executor(
+def apply_agent_executor_adapter(
     agent_executor: AgentExecutor,
     *,
     state_reader: Optional[Callable[[], Any]] = None,
 ) -> Runnable:
-    """An adapter for agent executor to standardize its output.
+    """An adapter for the agent executor to standardize its input and output.
 
-    1) Makes sure that `output` is always returned (will be set to "" if missing) --
-        Note that this should not be necessary after more updates in the eval.
-    2) Populate `state` with the system state if a state reader is provided.
+    1) Map `question` to `input` (`question` is used in the datasets,
+       but `input` is used in the agent executor)
+    2) Ensure that `output` is always returned (will be set to "" if missing) --
+       note that this may be relaxed after more updates in the eval config.
+    3) Populate `state` key in the response of the agent with the system state
+       if a state reader is provided.
 
     Args:
         agent_executor: the agent executor
@@ -131,7 +120,16 @@ def standardize_executor(
         else:
             return None
 
-    runnable = agent_executor | _ensure_output_exists
+    runnable = (
+        RunnablePassthrough.assign(input=lambda x: x["question"]).with_config(
+            {"run_name": "Format Input"}
+        )
+        | agent_executor
+        | RunnableLambda(_ensure_output_exists).with_config(
+            {"run_name": "Ensure Output"}
+        )
+    )
+
     if state_reader is not None:
         runnable = agent_executor | RunnablePassthrough.assign(
             state=_read_state
