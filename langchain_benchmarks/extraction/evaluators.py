bcee4c860dbd66a87b29067ee003cb92e10b7189
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-11-28 11:11:18 -0800

    Update evaluator and anthropic example

--- langchain_benchmarks/extraction/evaluators.py
+++ langchain_benchmarks/extraction/evaluators.py
@@ -6,7 +6,7 @@ def get_eval_config(eval_llm: BaseChatModel) -> RunEvalConfig:
     """Get the evaluation configuration for the email task."""
     return RunEvalConfig(
         evaluators=[
-            "json_dist",
+            "json_edit_distance",
             RunEvalConfig.LabeledScoreString(
                 criteria={
                     "accuracy": """
