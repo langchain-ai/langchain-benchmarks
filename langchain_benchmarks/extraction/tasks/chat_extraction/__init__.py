c4d85932935fb8937877dd40d99d00816e6c3f26
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-11-30 16:32:46 -0800

    Updating outputs

--- langchain_benchmarks/extraction/tasks/chat_extraction/__init__.py
+++ langchain_benchmarks/extraction/tasks/chat_extraction/__init__.py
@@ -1,12 +1,9 @@
 from langchain.prompts import ChatPromptTemplate as C
 
-from langchain_benchmarks.extraction.tasks.chat_extraction.evaluators import (
-    get_eval_config,
-)
 from langchain_benchmarks.extraction.tasks.chat_extraction.schema import generateTicket
 from langchain_benchmarks.schema import ExtractionTask
 
-# This is a default prompt that works for chat models.
+# This is a default prompt that works reasonably for OpenAI models.
 
 DEFAULT_CHAT_MODEL_PROMPT = C.from_messages(
     [
@@ -29,7 +26,10 @@ CHAT_EXTRACTION_TASK = ExtractionTask(
     name="Chat Extraction",
     dataset_id="https://smith.langchain.com/public/54d6d8e4-b420-4b9e-862d-548b1b65a6fe/d",
     schema=generateTicket,
-    description="""\
-    """,
+    description="""A dataset meant to test the ability of an LLM to extract and infer
+structured information from a dialogue. The dialogue is between a user and a support
+engineer. Outputs should be structured as a JSON object and test both the ability
+of the LLM to correctly structure the information and its ability to perform simple 
+classification tasks.""",
     instructions=DEFAULT_CHAT_MODEL_PROMPT,
 )
