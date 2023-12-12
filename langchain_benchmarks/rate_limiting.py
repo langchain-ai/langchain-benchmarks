a6624b418fa04729d34b7cc78427f33ae3dd5ebf
Eugene Yurtsev <eyurtsev@gmail.com>
2023-12-12 14:32:47 -0500

    x

--- langchain_benchmarks/rate_limiting.py
+++ langchain_benchmarks/rate_limiting.py
@@ -12,12 +12,15 @@ class RateLimiter:
         self,
         *,
         requests_per_second: float = 1,
-        tokens_per_request: int = 1,
         check_every_n_seconds: float = 0.1,
+        max_bucket_size: Optional[int] = 1,
     ) -> None:
         """A rate limiter based on a token bucket.
 
-        This is a rate limiter which works in a threaded environment.
+        These *tokens* have NOTHING to do with LLM tokens. They are just
+        a way to keep track of how many requests can be made.
+
+        This rate limiter is designed to work in a threaded environment.
 
         It works by filling up a bucket with tokens at a given rate. Each
         request consumes a given number of tokens. If there are not enough
@@ -27,27 +30,27 @@ class RateLimiter:
         Args:
             requests_per_second: The number of tokens to add per second to the bucket.
                 Must be at least 1.
-            tokens_per_request: number of tokens that a request "costs".
-                This is 1 by default.
             check_every_n_seconds: check whether the tokens are available
                 every this many seconds. Can be a float to represent
                 fractions of a second.
+            max_bucket_size: The maximum number of tokens that can be in the bucket.
+                This is used to prevent bursts of requests.
         """
+
         if requests_per_second < 1:
             raise ValueError("Rate must be at least 1 request per second")
         self.requests_per_second = requests_per_second
         # Number of tokens in the bucket.
         self.available_tokens = 0.0
+        self.max_bucket_size = max_bucket_size
         # A lock to ensure that tokens can only be consumed by one thread
         # at a given time.
         self._consume_lock = threading.Lock()
-        # tokens per request sets how many tokens
-        self.tokens_per_request = tokens_per_request
         # The last time we tried to consume tokens.
         self.last: Optional[time.time] = None
         self.check_every_n_seconds = check_every_n_seconds
 
-    def consume(self) -> bool:
+    def _consume(self) -> bool:
         """Consume the given amount of tokens if possible.
 
         Returns:
@@ -68,17 +71,18 @@ class RateLimiter:
                 self.available_tokens += elapsed * self.requests_per_second
                 self.last = now
 
-            self.available_tokens = min(self.available_tokens, self.requests_per_second)
+            # Make sure that we don't exceed the bucket size.
+            self.available_tokens = min(self.available_tokens, self.max_bucket_size)
 
-            if self.available_tokens >= self.tokens_per_request:
-                self.available_tokens -= self.tokens_per_request
+            if self.available_tokens >= 1:
+                self.available_tokens -= 1
                 return True
 
             return False
 
     def wait(self) -> None:
         """Blocking call to wait until the given number of tokens are available."""
-        while not self.consume():
+        while not self._consume():
             time.sleep(self.check_every_n_seconds)
 
 
@@ -96,12 +100,9 @@ def with_rate_limit(
         A runnable lambda that acts as a throttled passthrough.
     """
 
-    def _rate_limited_passthrough(input: dict, **kwargs: Any) -> dict:
-        """Throttle the input."""
+    def _wait(input: dict, **kwargs: Any) -> dict:
+        """Wait for the rate limiter to allow the request to proceed."""
         rate_limiter.wait()
         return input
 
-    return (
-        RunnableLambda(_rate_limited_passthrough).with_config({"name": "Rate Limit"})
-        | runnable
-    )
+    return RunnableLambda(_wait).with_config({"name": "Wait"}) | runnable
